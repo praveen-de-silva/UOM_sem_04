d502e8b8783ea2b861b98e4392ba05495a16d08e
Praveen De Silva <desilvabkp.23@uom.lk>
2026-03-25 21:47:43 +0530

    Add model training and accuracy evaluation to HousePricePred script

--- CS_ML/Excercises/House/HousePricePred_.py
+++ CS_ML/Excercises/House/HousePricePred_.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score
 
 # -----------------
 #     Load Data
@@ -24,8 +25,16 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=42
 )
 
-# --------------------------
-#     Data 
-# --------------------------
+# ----------------------
+#     Model Training
+# ----------------------
 
 model = RandomForestRegressor()
+model.fit(X_train, y_train)
+
+# -----------------------
+#     Model Valdating
+# -----------------------
+
+y_pred = model.predict(X_test, y_test)
+print(f"Accuracy = {accuracy_score(y)}")
